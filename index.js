d3877075d6672c87b4ba2fbecf47305f74ffe75e
Angela <angelaayodeji@gmail.com>
2026-03-12 20:30:36 -0500

    figured out winning line transformation

--- index.js
+++ index.js
@@ -31,25 +31,84 @@ const turnManager = (() =>{
 })();
 
 const boardManger = (()=>{
-
-
+    const drawWinLine = ()=>{
+        const line = document.querySelector(".winningLine")
+        const winCombo = winManager.determineWinner();
+        switch(winCombo.join()){
+            case "0,1,2":
+                line.classList.add("top-row");
+                break;
+            case "3,4,5":
+                line.classList.add("middle-row");
+                break;
+            case "6,7,8":
+                line.classList.add("bottom-row");
+                break;
+            case "0,3,6":
+                line.classList.add("left-column");
+                break;
+            case "1,4,7":
+                line.classList.add("middle-column");
+                break;
+            case "2,5,8":
+                line.classList.add("right-column");
+                break;
+            case "0,4,8":
+                line.classList.add("left-diag");
+                break;
+            case "2,4,6":
+                line.classList.add("right-diag");
+                break;
+                
+        }
+    }
 })();
 
 const gameLogic = (()=>{
     let gamePad = [ "null", "null", "null",
                       "null", "null", "null",
                       "null", "null", "null"]
+    let rounds = 0;
+    const updateRounds = ()=>{ rounds++;}
+    const getRounds = ()=> rounds;
     const updateGamePad = (index, content)=>{
         gamePad[index] = content;
     }
 
     const getGamePad = () => gamePad;
 
-    return{updateGamePad, getGamePad};
+    return{updateGamePad, getGamePad, updateRounds, getRounds};
+})();
+
+const winManager = (()=>{
+    let winningCombo = [
+        //rows
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        //columns
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        //diagonal
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    const determineWinner = () =>{
+        let gameBoard = gameLogic.getGamePad();
+        //remember that currentPlayer holds the fruit in play
+        let currentPlayer = turnManager.getCurrentTurn();
+        const winner = winningCombo.find(combo => combo.every(i => gameBoard[i] == currentPlayer));
+
+        return winner;
+    }
+
+    return{determineWinner};
 })();
 
 const startGame = (() =>{
-    const gamePlay = document.querySelector("#gamePlay");
+    const gamePlay = document.querySelector("#gameGrid");
     const generateBoard = () =>{
         for(let i=0; i<9; i++){
             const square = document.createElement("div");
@@ -59,8 +118,14 @@ const startGame = (() =>{
             square.addEventListener("click", ()=>{
                 square.textContent = turnManager.getCurrentTurn();
                 gameLogic.updateGamePad(square.id, turnManager.getCurrentTurn());
+                gameLogic.updateRounds();
+                if(gameLogic.getRounds() >= 5){
+                    if(winManager.determineWinner()){
+                        console.log(gameLogic.getGamePad());
+                    }
+                }
                 turnManager.findTurn();
-                console.log(gameLogic.getGamePad());
+                //console.log(gameLogic.getGamePad());
                 
             }, {once: true});
             
